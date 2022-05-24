5ffca15cd9f0b0ff24af2dea826b031d7e347a60
ManoelSobrinho <cleonaldosobrinho@hotmail.com>
2022-05-24 20:55:03 -0300

    Update QueryTimeExecution.sql

--- QueryTimeExecution.sql
+++ QueryTimeExecution.sql
@@ -3,23 +3,23 @@ GO
 
 SELECT * FROM Person.Person
 
--- Utilizando SET STATISTICS IO, TIME ON
+-- Utilizando SET STATISTICS TIME ON
 
-SET STATISTICS IO, TIME ON
+SET STATISTICS TIME ON
 SELECT * FROM Person.Person
-SET STATISTICS IO, TIME OFF
+SET STATISTICS TIME OFF
 GO
  
 -- Utilizando GETDATE() subtraindo o tempo final menos o inicial - medidas em milisegundos (1s / 1000)
 
 DECLARE @inicio DATETIME = GETDATE()
 SELECT * FROM Person.Person
-PRINT CONCAT('ConcluÌdo: ', RIGHT(CONCAT('0', DATEDIFF(d, @inicio, SYSDATETIME())), 2), ' dias ', + CONVERT(VARCHAR, GETDATE() - @inicio, 114))
+PRINT CONCAT('Conclu√≠do: ', RIGHT(CONCAT('0', DATEDIFF(d, @inicio, SYSDATETIME())), 2), ' dias ', + CONVERT(VARCHAR, GETDATE() - @inicio, 114))
 GO
  
--- Utilizando SYSDATETIME() - medidas em microsegundos (1s / 1 milh„o) (maior precis„o)
+-- Utilizando SYSDATETIME() - medidas em microsegundos (1s / 1 milh√£o) (maior precis√£o)
 
 DECLARE @inicio DATETIME2 = SYSDATETIME()
 SELECT * FROM Person.Person
-PRINT CONCAT('ConcluÌdo: ', RIGHT(CONCAT('0', DATEDIFF(d, @inicio, SYSDATETIME())), 2), ' dias ', CONVERT(VARCHAR, CONVERT(DATETIME, SYSDATETIME()) - CONVERT(DATETIME, @inicio), 108), '.', RIGHT('000000' + CONVERT(VARCHAR, CONVERT(BIGINT, DATEDIFF_BIG(MICROSECOND, @inicio, SYSDATETIME()) % 1000000.0)), 6))
+PRINT CONCAT('Conclu√≠do: ', RIGHT(CONCAT('0', DATEDIFF(d, @inicio, SYSDATETIME())), 2), ' dias ', CONVERT(VARCHAR, CONVERT(DATETIME, SYSDATETIME()) - CONVERT(DATETIME, @inicio), 108), '.', RIGHT('000000' + CONVERT(VARCHAR, CONVERT(BIGINT, DATEDIFF_BIG(MICROSECOND, @inicio, SYSDATETIME()) % 1000000.0)), 6))
 GO
