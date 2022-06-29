719c7f1983186eca0fa5675c3aaa520746894cda
ManoelSobrinho <cleonaldosobrinho@hotmail.com>
2022-06-29 20:46:52 -0300

    Add files via upload

---
+++ InstanceInformation.sql
@@ -0,0 +1,12 @@
+SELECT 
+	SERVERPROPERTY('ServerName') AS ServerName,
+	SERVERPROPERTY('InstanceName') AS InstanceName,
+	CONNECTIONPROPERTY('local_net_address') AS IP,
+	SERVERPROPERTY('Edition') AS SQLEdition,
+	SERVERPROPERTY('IsClustered') AS IsClustered,
+	SERVERPROPERTY('IsHadrEnabled') AS IsHadrEnabled,
+	@@VERSION AS SQLVersion,
+	cpu_count AS CPUs,
+	physical_memory_kb/1024 AS RAM
+FROM sys.dm_os_sys_info
+GO
