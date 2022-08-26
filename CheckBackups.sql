9464f64b4913f40209c7a2475f8f150de5139708
ManoelSobrinho <cleonaldosobrinho@hotmail.com>
2022-08-26 08:18:44 -0300

    Add files via upload

---
+++ CheckBackups.sql
@@ -0,0 +1,8 @@
+SELECT database_name, name, backup_start_date, DATEDIFF(mi,backup_start_date,backup_finish_date) [tempo (min)],
+	position, server_name, recovery_model, last_lsn,
+	ISNULL(logical_device_name,' ') logical_device_name,device_type, type, CAST(backup_size/1024/1024 AS NUMERIC(15,2)) [Tamanho (MB)],first_lsn
+FROM msdb.dbo.backupset B
+	INNER JOIN msdb.dbo.backupmediafamily BF ON B.media_set_id = BF.media_set_id
+WHERE backup_start_date >= DATEADD(hh, -24 ,GETDATE()  )
+	AND database_name = 'Nome_Da_Base'
+ORDER BY backup_start_date DESC
