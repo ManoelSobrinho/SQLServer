e2fc6e4cb29ee77de6e52bc08ca1d4fd6b3e55f8
ManoelSobrinho <cleonaldosobrinho@hotmail.com>
2022-08-26 09:03:35 -0300

    Update CheckBackups.sql

--- CheckBackups.sql
+++ CheckBackups.sql
@@ -4,5 +4,5 @@ SELECT database_name, name, backup_start_date, DATEDIFF(mi,backup_start_date,bac
 FROM msdb.dbo.backupset B
 	INNER JOIN msdb.dbo.backupmediafamily BF ON B.media_set_id = BF.media_set_id
 WHERE backup_start_date >= DATEADD(hh, -24 ,GETDATE()  )
-	AND database_name = 'Nome_Da_Base'
+	AND database_name = 'DatabaseName'
 ORDER BY backup_start_date DESC
