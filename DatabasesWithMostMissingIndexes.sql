f00d629fc4d315b18ce8522d52997b65d94be580
ManoelSobrinho <cleonaldosobrinho@hotmail.com>
2025-04-23 15:18:11 -0300

    Add files via upload

---
+++ DatabasesWithMostMissingIndexes.sql
@@ -0,0 +1,8 @@
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+SELECT
+	DB_NAME(database_id) AS DatabaseName
+	, COUNT(*) AS [Missing Index Count]
+FROM sys.dm_db_missing_index_details
+GROUP BY DB_NAME(database_id)
+ORDER BY [Missing Index Count] DESC
